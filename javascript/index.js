27ed372731bbaf8fc91dfe8ec6752f652d60bf53
Volker Jacobsen <volkerjacobsen@gmail.com>
2023-05-26 09:27:13 +0200

    add darkmode

--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,24 @@
-const dark_mode_button = document.getElementById("dark_mode_button");
-dark_mode_button.addEventListener("Click", () => {
-  document.body.classList.toffle("dark_mode");
-});
+const toggleSwitch = document.querySelector(
+  '.theme-switch input[type="checkbox"]'
+);
+const currentTheme = localStorage.getItem("theme");
+
+if (currentTheme) {
+  document.documentElement.setAttribute("data-theme", currentTheme);
+
+  if (currentTheme === "dark") {
+    toggleSwitch.checked = true;
+  }
+}
+
+function switchTheme(e) {
+  if (e.target.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark");
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem("theme", "light");
+  }
+}
+
+toggleSwitch.addEventListener("change", switchTheme, false);
